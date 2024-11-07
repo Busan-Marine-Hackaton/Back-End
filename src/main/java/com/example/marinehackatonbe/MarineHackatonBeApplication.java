9fb7c0cbe56b65b3c6095cd08bd3ef1a5f47b28c
codrin2 <hssang127@gmail.com>
2024-11-07 22:21:41 +0900

    initial setting

---
+++ src/main/java/com/example/marinehackatonbe/MarineHackatonBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.marinehackatonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarineHackatonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarineHackatonBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marinehackatonbe/MarineHackatonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marinehackatonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarineHackatonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
