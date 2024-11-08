b5c57a8775ef2d69ca5ac7b710a819215f7faee6
codrin2 <hssang127@gmail.com>
2024-11-08 21:10:30 +0900

    feat: exception 처리 및 commonResponse 추가

---
+++ src/main/java/com/example/marinehackatonbe/global/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.example.marinehackatonbe.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+	private final ExceptionContent content;
+
+	@Override
+	public String getMessage() {
+		return content.getMessage();
+	}
+}
---
+++ src/main/java/com/example/marinehackatonbe/global/exception/ExceptionContent.java
@@ -0,0 +1,19 @@
+package com.example.marinehackatonbe.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Getter
+@RequiredArgsConstructor
+public enum ExceptionContent {
+	NOT_FOUND_MEMBER(NOT_FOUND, "멤버가 존재하지 않습니다." ),
+	NOT_FOUND_PRODUCT(NOT_FOUND, "상품이 존재하지 않습니다." ),
+	WRONG_PASSWORD(NOT_FOUND, "잘못된 패스워드 입니다." ),
+	FAIL_PHOTO_UPLOAD(NOT_FOUND, "사진 업로드에 실패하였습니다." );
+
+	private final HttpStatus httpStatus;
+	private final String message;
+}
